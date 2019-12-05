3ef181dd7e3f000bd114c896fc0858f130514c21
Murilo Pupo de Oliveira <48927673+mpupo@users.noreply.github.com>
2019-12-05 07:40:36 -0300

    Finalizando o desafio

--- desafio.c
+++ desafio.c
@@ -6,15 +6,15 @@ Escreva um programa que tem a estrutura de dados abaixo:
 - Salário.
 
 O programa tem as seguintes opções de menu:
-1.  Entra dados.
-2.  Lista dados na tela.
-3.  pesq_por_nomer um registro por nome.
-4.  pesq_por_nome por estado civil
-5.  pesq_por_nomer os aniversariantes de um determinado mês.
-6.  pesq_por_nome por faixa salarial
-7.  Altera valor do salário
-8.  Altera dados.
-9.  Exclui dados.
+1.  Entrar dados.
+2.  Listar dados na tela.
+3.  Pesquisar um usuário por nome.
+4.  Pesquisar um usuário por estado civil
+5.  Pesquisar os aniversariantes de um determinado mês.
+6.  Pesquisar um usuário por faixa salarial
+7.  Alterar valor do salário de um usuário
+8.  Alterar dados de um usuário.
+9.  Excluir dados de um usuário.
 10. Saída
 
 Todas as operações devem ser feitas direto no arquivo. (utilize a função fseek).
@@ -38,7 +38,7 @@ struct dados
     float salario;
 };
 
-void inclui_usuario(struct dados *inf_a_gravar, int tam);
+void incluir_usuario(struct dados *inf_a_gravar, int tam);
 void listar_usuarios(struct dados *inf_a_gravar, int tam);
 int pesq_por_nome(struct dados *inf_a_gravar, int tam);
 int pesq_por_ec(struct dados *inf_a_gravar, int tam);
@@ -51,11 +51,10 @@ void excluir_usuario(struct dados *inf_a_gravar, int tam);
 int main()
 {
 
-    struct dados infos;
-    struct dados *p_infos;
-    int operacao;
-    int i;
-    int tam; //n. de bytes da estrutura
+    struct dados infos;    // Estrutura de dados.
+    struct dados *p_infos; // Ponteiro para a struct de informações do usuário.
+    int operacao;          // Operacao a ser realizada pelo usuario.
+    int tam;               // N. de bytes da estrutura.
 
     p_infos = &infos;
     tam = sizeof(infos);
@@ -71,7 +70,7 @@ int main()
         printf(" 3 -Pesquisar por nome \n");
         printf(" 4 -Pesquisar por estado civil\n");
         printf(" 5 -Pesquisar por aniversario\n");
-        printf(" 6 -Pesquisr por salario\n");
+        printf(" 6 -Pesquisar por salario\n");
         printf(" 7 -Alterar valor do salário de um usuario\n");
         printf(" 8 -Alterar dados de um usuario\n");
         printf(" 9 -Excluir usuario\n");
@@ -82,7 +81,7 @@ int main()
         switch (operacao)
         {
         case 1:
-            inclui_usuario(p_infos, tam); //passa como parametro o ponteiro para a estrutura e o n. de bytes da desta
+            incluir_usuario(p_infos, tam); //passa como parametro o ponteiro para a estrutura e o n. de bytes da desta
 
             system("cls");
             break;
@@ -105,15 +104,15 @@ int main()
             break;
 
         case 5:
-            //pesq_por_aniv(p_infos, tam);
+            pesq_por_aniv(p_infos, tam);
             break;
 
         case 6:
-            //pesq_por_sal(p_infos, tam);
+            pesq_por_sal(p_infos, tam);
             break;
 
         case 7:
-            //alterar_salario(p_infos, tam);
+            alterar_salario(p_infos, tam);
             break;
 
         case 8:
@@ -134,7 +133,7 @@ int main()
     };
 }
 
-void inclui_usuario(struct dados *inf_a_gravar, int tam)
+void incluir_usuario(struct dados *inf_a_gravar, int tam)
 {
     FILE *arquivo, *arq_contador;
     int cont = 0;
@@ -378,6 +377,196 @@ int pesq_por_ec(struct dados *inf_a_gravar, int tam)
     return registro_escolhido;
 }
 
+int pesq_por_aniv(struct dados *inf_a_gravar, int tam)
+{
+    FILE *arquivo;
+    FILE *arq_contador;
+
+    int mes_aniv;
+    int cont, letra;
+    int registro_escolhido;
+    int achou = 0;
+
+    system("cls");
+
+    printf("**********************************************************************************\n");
+    printf("*                PESQUISA DE USUARIO POR ANIVERSARIO                             *\n");
+    printf("**********************************************************************************\n");
+
+    printf("Informe um mes (MM) para pesquisar usuarios que facam aniversario:");
+    scanf("%d", &mes_aniv);
+    fflush(stdin);
+
+    arq_contador = fopen("contador.txt", "r");
+    fscanf(arq_contador, "%d", &cont);
+    fclose(arq_contador);
+
+    arquivo = fopen("usuarios.txt", "r");
+
+    printf("\nN. Reg\t| Nome\t\t| Estado Civil\t\t| Data de Nascimento \t| Salario \t|\n");
+    printf("---------------------------------------------------------------------------------\n");
+
+    for (int registro = 0; registro < cont; registro++)
+    {
+        fread(inf_a_gravar, tam, 1, arquivo);
+
+        if (mes_aniv != inf_a_gravar->mes_nasc)
+        {
+            continue;
+        }
+
+        else
+        {
+            printf("%-5d\t %-16s %-10s\t\t %5d/%d/%d\t\t %10.2f\n\n", registro, inf_a_gravar->nome, inf_a_gravar->estado_civil, inf_a_gravar->dia_nasc, inf_a_gravar->mes_nasc, inf_a_gravar->ano_nasc, inf_a_gravar->salario);
+            achou = 1;
+        }
+    }
+
+    fclose(arquivo);
+
+    if (!achou)
+    {
+        printf("Nao existem registros com o mes solicitado!\n");
+        printf("Digite enter para voltar ao menu:");
+        getchar();
+
+        system("cls");
+        return -1;
+    }
+
+    printf("Digite o numero do registro do usuario selecionado:");
+    scanf("%d", &registro_escolhido);
+    getchar();
+
+    printf("Digite enter para voltar ao menu:");
+    getchar();
+
+    system("cls");
+
+    return registro_escolhido;
+}
+
+int pesq_por_sal(struct dados *inf_a_gravar, int tam)
+{
+    FILE *arquivo;
+    FILE *arq_contador;
+
+    int sal_min, sal_max;
+    int cont, letra;
+    int registro_escolhido;
+    int achou = 0;
+
+    system("cls");
+
+    printf("**********************************************************************************\n");
+    printf("*                PESQUISA DE USUARIO POR SALARIO                             *\n");
+    printf("**********************************************************************************\n");
+
+    printf("Informe uma faixa salarial para realizar a pesquisa dos usuarios.\n");
+    printf("Limite minimo:");
+    scanf("%d", &sal_min);
+    fflush(stdin);
+
+    printf("Limite maximo:");
+    scanf("%d", &sal_max);
+    fflush(stdin);
+
+    arq_contador = fopen("contador.txt", "r");
+    fscanf(arq_contador, "%d", &cont);
+    fclose(arq_contador);
+
+    arquivo = fopen("usuarios.txt", "r");
+
+    printf("\nN. Reg\t| Nome\t\t| Estado Civil\t\t| Data de Nascimento \t| Salario \t|\n");
+    printf("---------------------------------------------------------------------------------\n");
+
+    for (int registro = 0; registro < cont; registro++)
+    {
+        fread(inf_a_gravar, tam, 1, arquivo);
+
+        if (inf_a_gravar->salario < sal_min || inf_a_gravar->salario > sal_max)
+        {
+            continue;
+        }
+
+        else
+        {
+            printf("%-5d\t %-16s %-10s\t\t %5d/%d/%d\t\t %10.2f\n\n", registro, inf_a_gravar->nome, inf_a_gravar->estado_civil, inf_a_gravar->dia_nasc, inf_a_gravar->mes_nasc, inf_a_gravar->ano_nasc, inf_a_gravar->salario);
+            achou = 1;
+        }
+    }
+
+    fclose(arquivo);
+
+    if (!achou)
+    {
+        printf("Nao existem registros com o limite solicitado!\n");
+        printf("Digite enter para voltar ao menu:");
+        getchar();
+
+        system("cls");
+        return -1;
+    }
+
+    printf("Digite o numero do registro do usuario selecionado:");
+    scanf("%d", &registro_escolhido);
+    getchar();
+
+    printf("Digite enter para voltar ao menu:");
+    getchar();
+
+    system("cls");
+
+    return registro_escolhido;
+}
+
+void alterar_salario(struct dados *inf_a_gravar, int tam)
+{
+    FILE *arquivo;
+    int n_reg;
+    int n_bytes;
+
+    // Pesquisa pelo nome o registro do usuario no arquivo:
+    n_reg = pesq_por_nome(inf_a_gravar, tam);
+
+    // Se o registro for encontrado:
+    if (n_reg != -1)
+    {
+        // Determina o numero de bits para posterior uso na fseek:
+        n_bytes = tam * n_reg;
+
+        arquivo = fopen("usuarios.txt", "r+");
+
+        // Posiciona o ponteiro do arquivo no inicio do regisro a ser alterado:
+        fseek(arquivo, n_bytes, 0);
+
+        // Le o registro do arquivo:
+        fread(inf_a_gravar, tam, 1, arquivo);
+
+        // Novos dados para o registro:
+        do
+        {
+            printf("Digite o novo salario do usuario:");
+            scanf("%f", &inf_a_gravar->salario);
+            fflush(stdin);
+        } while (inf_a_gravar->salario < 0);
+
+        // Posiciona o ponteiro do arquivo no inicio do regisro a ser alterado:
+        fseek(arquivo, n_bytes, 0);
+
+        // Escreve o registro no arquivo:
+        fwrite(inf_a_gravar, tam, 1, arquivo);
+
+        fclose(arquivo);
+
+        printf("\nUsuario alterado!\n");
+        printf("Digite enter para confirmar e voltar ao menu:");
+        getchar();
+
+        system("cls");
+    }
+}
+
 void alterar_usuario(struct dados *inf_a_gravar, int tam)
 {
     FILE *arquivo;
@@ -395,7 +584,8 @@ void alterar_usuario(struct dados *inf_a_gravar, int tam)
 
         arquivo = fopen("usuarios.txt", "r+");
 
-        fseek(arquivo, n_bytes, 0); //posioiona o ponteiro do arquivo no registro a ser alterado
+        // Posiciona o ponteiro do arquivo no inicio do regisro a ser alterado:
+        fseek(arquivo, n_bytes, 0);
 
         // Le o registro do arquivo:
         fread(inf_a_gravar, tam, 1, arquivo);
@@ -450,23 +640,29 @@ void alterar_usuario(struct dados *inf_a_gravar, int tam)
 
 void excluir_usuario(struct dados *inf_a_gravar, int tam)
 {
-    FILE *p;
-    p = fopen("arquivo.txt", "r+");
+    FILE *arquivo;
+    arquivo = fopen("usuarios.txt", "r+");
     int n_reg;
     int n_bytes;
 
-    n_reg = pesq_por_nome(inf_a_gravar, tam); //pesq_por_nome o registro no arquivo
+    // Pesquisa pelo nome o registro do usuario no arquivo:
+    n_reg = pesq_por_nome(inf_a_gravar, tam);
+
+    // Determina o numero de bits para posterior uso na fseek:
     n_bytes = tam * n_reg;
 
-    fseek(p, n_bytes, 0);           //posioiona o ponteiro do arquivo no registro a ser apagado
-    fread(inf_a_gravar, tam, 1, p); //le o registro do arquivo
+    // Posiciona o ponteiro do arquivo no inicio do regisro a ser alterado:
+    fseek(arquivo, n_bytes, 0);
+
+    // Le o registro do arquivo:
+    fread(inf_a_gravar, tam, 1, arquivo);
     printf("nome para apagar e' %s\n", inf_a_gravar->nome);
     //apaga o registro do arquivo
 
     printf("nome para apagar e' %s\n", inf_a_gravar->nome);
 
-    fseek(p, n_bytes, 0);            //posiciona o ponteiro do arquivo no inicio do regisro a ser apagado
-    fwrite(inf_a_gravar, tam, 1, p); //escreve o registro
+    fseek(arquivo, n_bytes, 0);            //posiciona o ponteiro do arquivo no inicio do regisro a ser apagado
+    fwrite(inf_a_gravar, tam, 1, arquivo); //escreve o registro
 
-    fclose(p);
+    fclose(arquivo);
 }
